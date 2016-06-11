b918ff9da08b965c89357a734c8964a655c1ff0d
Dilshat Hewzulla <hewzulla@gmail.com>
2016-06-11 12:41:54 +0100

    video uploaded to s3 anc create video in brightcove

---
+++ config_bc.sh
@@ -0,0 +1,37 @@
+bc_config_filepath="$HOME/.bcs3uploader/credeantials"
+
+if [ -f $bc_config_filepath ]; then
+	. $bc_config_filepath
+else    
+    mkdir ".bcs3uploader"	   
+fi
+
+
+read -p "Brightcove API Account ID[$bc_account_id]:" bc_id
+read -p "Brightcove API Access Key[$bc_access_key_id]:" bc_key
+read -p "Brightcove API Access Secret[$bc_secret_access_key]:" bc_secret
+
+if [ "$bc_id" == "" ]; then
+   echo   
+else
+   bc_account_id=$bc_id
+fi
+
+
+
+if [ "$bc_key" == "" ]; then
+   echo   
+else
+  bc_access_key_id=$bc_key
+fi
+ 
+if [ "$bc_secret" == "" ]; then
+   echo 
+else
+  bc_secret_access_key=$bc_secret
+fi
+
+echo "bc_account_id=$bc_account_id" > $bc_config_filepath
+echo "bc_access_key_id=$bc_access_key_id" >> $bc_config_filepath
+echo "bc_secret_access_key=$bc_secret_access_key" >> $bc_config_filepath
+
---
+++ trim_upload.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+scriptbasedir=$(dirname "$0")
+
+bc_oauth_url=https://oauth.brightcove.com/v3
+bc_cmsurl=https://cms.api.brightcove.com/v1
+
+
+template_file_for_create_video="$scriptbasedir/template-file-for-create-video.json"
+bc_config_filepath="$HOME/.bcs3uploader/credeantials"
+
+inputvideo_filepath=$1
+inputvideo_extension="${inputvideo_filepath##*.}"
+inputvideo_basefilepath="${inputvideo_filepath%.*}"
+inputvideo_filename=${inputvideo_filepath##*/}
+inputvideo_filename_base="${inputvideo_filename%.*}"
+inputvideo_trimmedfilepath=$inputvideo_basefilepath'___trimmed.'$inputvideo_extension
+inputvideo_title=$inputvideo_filename_base
+timestamp=$(date +%s)
+bcuploader_logfile="/tmp/bcuploader_logfile.log"
+now=`date`
+
+log(){
+	logContent=$1
+    echo $1 >> $bcuploader_logfile
+}
+intLog(){
+	if [ -d /tmp ]; then
+  		echo 
+	else
+  		mkdir /tmp
+	fi
+	
+	if [ -f $bcuploader_logfile ]; then  
+        echo "***********$now*************" >> $bcuploader_logfile
+	else
+  		echo "***********$now*************" > $bcuploader_logfile
+    fi
+}
+load_config(){
+   . $bc_config_filepath
+}
+
+request_token(){
+    bc_token_request_post_body="grant_type=client_credentials"
+	token_request_command='curl -X POST --user "'$bc_access_key_id':'$bc_secret_access_key'" --data '$bc_token_request_post_body' '$bc_oauth_url'/access_token'
+    
+    token_response_body=$(eval $token_request_command)
+    log "token_response_body:$token_response_body"
+ 
+    bc_access_token=$(echo "$token_response_body" | sed 's/.*access_token\"\:\"//' |sed 's/\".*//')
+}
+create_video_placeholder(){
+    create_video_request_body=`cat $template_file_for_create_video`	
+	create_video_request_body=$(echo $create_video_request_body | sed "s/###ingest-video-title###/${inputvideo_title}/g")    
+    create_video_request_tmpfile=$(mktemp /tmp/create-video-$inputvideo_filename_base.json)
+    
+    log "video request tmp file:$create_video_request_tmpfile"    
+    
+    echo "$create_video_request_body" > $create_video_request_tmpfile
+    log "create_video_request_body:$create_video_request_body"
+               
+    create_video_command="curl  -X POST -H \"Authorization: Bearer $bc_access_token\"    -d @$create_video_request_tmpfile      $bc_cmsurl/accounts/$bc_account_id/videos"
+        
+    log "create_video_command:$create_video_command"
+        
+    create_video_response_body=$(eval $create_video_command)
+    
+    log "create_video_response_body:$create_video_response_body"    
+        
+    bc_video_id=$(echo "$create_video_response_body" | grep "\"id\"\s*:"| sed -e 's/.*\"id\" *\: *\"//' | sed 's/\".*//')
+    
+    log "bc_video_id:$bc_video_id"
+}
+trimvideo(){
+   log "trmming video $inputvideo_filepath to $inputvideo_trimmedfilepath"
+   ffmpeg -ss 10 -i $inputvideo_filepath $inputvideo_trimmedfilepath
+}
+s3uplad(){
+   log "Uploading the video to s3://box-video/$inputvideo_filename"
+   aws s3 cp $inputvideo_trimmedfilepath s3://box-video/$inputvideo_filename
+}
+
+intLog
+log "Input file: $inputvideo_filepath "
+log "Video title: $inputvideo_title "
+
+
+if [ -f $bc_config_filepath ]; then	
+	load_config
+	request_token
+	create_video_placeholder
+    	
+else   
+    echo "******************Warning**********" 
+    echo "The brightcove configuration is not found, so will not ingest the video file into the brightcove video cloud."
+    echo "But will try to upload the video file to the s3 bucket"
+    echo "Please obtain the brightcove video cloud api key and secret from the brightcove video cloud studio, and then run the following command:"
+    echo "./config_bc.sh"	   
+    echo "******************Warning**********"
+    log "The brightcove configuration is not found, skipping brightcove process"
+fi
+
+trimvideo
+s3uplad
+
+
+
+ 
+
+
+
+
+
+  
+
+
+
