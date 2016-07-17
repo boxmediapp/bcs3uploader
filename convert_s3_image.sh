c3b9b512e107aab808224aba13e6e0b8c249e9d0
Dilshat Hewzulla <hewzulla@gmail.com>
2016-07-17 18:06:14 +0100

    developing

--- convert_s3_image.sh
+++ convert_s3_image.sh
@@ -4,7 +4,61 @@ scriptbasedir=$(dirname "$0")
 bucketname=$1
 filepath=$2
 filename=$3
+destfolder=$4
+
+file_extension="${filename##*.}"
+base_filename="${filename%.*}"
+
+
 
 cd /data
-aws s3 cp s3://bucketname/$filepath  $filename
+
+aws s3 cp s3://$bucketname/$filepath  $filename
+
+convert(){
+
+width=$1
+height=$2
+
+
+destfilename=$base_filename'_'$width'x'$height'.'$file_extension
+
+commandString='ffmpeg -y -i '$filename' -vf scale='$width':'$height' '$destfilename
+echo "Executing:: $commandString"
+$commandString
+ 
+commandString='aws s3 cp '$destfilename' s3://'$bucketname'/'$destfolder'/'$destfilename
+echo "Executing:: $commandString"
+$commandString
+rm $destfilename
+}
+
+
+convert 288 162
+convert 256 144
+convert 320 180
+convert 400 225
+convert 432 243
+convert 512 288
+convert 640 360
+convert 786 406
+convert 824 426
+convert 1089 563
+convert 1160 508
+convert 1280 720
+convert 1920 1080
+
+rm $filename
+
+
+
+
+
+
+
+ 
+
+
+
+
 
