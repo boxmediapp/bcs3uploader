0ad3b01717364a91f95b5046c92aa4687c68cca3
Dilshat Hewzulla <hewzulla@gmail.com>
2016-11-03 23:57:46 +0000

    move-to-mo4

---
+++ mov_to_mp4_to_s3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+scriptbasedir=$(dirname "$0")
+
+bucketname=$1
+filepath=$2
+filename=$3
+destfolder=$4
+
+file_extension="${filename##*.}"
+base_filename="${filename%.*}"
+
+
+
+cd /data
+echo "executing::aws s3 cp s3://$bucketname/$filepath  $filename"
+
+aws s3 cp s3://$bucketname/$filepath  $filename
+
+
+convert(){
+
+
+destfilename=$base_filename'.mp4'
+
+
+
+commandString='ffmpeg -i '$filename' -y -preset medium -r 25 -s 1024x576 -aspect 16:9 -vcodec libx264 -b:v 11M -profile:v high -level:v 3.1 -vf "yadif=0:-1:0" -af dynaudnorm=f=24:g=31:p=.70:m=20:r=1:n=1:c=1:b=0 -ar 48000 -ac 2 -acodec aac -ab 128k '$destfilename
+echo "Executing:: $commandString"
+$commandString
+ 
+commandString='aws s3 cp '$destfilename' s3://'$bucketname'/'$destfolder'/'$destfilename
+$commandString
+echo "Executing:: $commandString"
+rm $destfilename
+}
+
+
+convert
+
+rm $filename
+
