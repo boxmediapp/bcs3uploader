ffbce5c14441cfe71d59f0dfbacf84d8a19d6ce4
Dilshat Hewzulla <hewzulla@gmail.com>
2016-11-13 00:40:26 +0000

    developing

--- mov_to_mp4_to_s3.sh
+++ mov_to_mp4_to_s3.sh
@@ -24,7 +24,8 @@ destfilename=$base_filename'.mp4'
 
 
 
-commandString='ffmpeg -i '$filename' -y -preset medium -r 25 -s 1024x576 -aspect 16:9 -vcodec libx264 -b:v 11M -profile:v high -level:v 3.1 -vf "yadif=0:-1:0" -af dynaudnorm=f=24:g=31:p=.70:m=20:r=1:n=1:c=1:b=0 -ar 48000 -ac 2 -acodec aac -ab 128k '$destfilename
+#commandString='ffmpeg -i '$filename' -y -preset medium -r 25 -s 1024x576 -aspect 16:9 -vcodec libx264 -b:v 11M -profile:v high -level:v 3.1 -vf "yadif=0:-1:0" -af dynaudnorm=f=24:g=31:p=.70:m=20:r=1:n=1:c=1:b=0 -ar 48000 -ac 2 -acodec aac -ab 128k '$destfilename
+commandString='ffmpeg -i '$filename' -y -vcodec copy -af dynaudnorm=f=24:g=31:p=.70:m=20:r=1:n=1:c=1:b=0 -ar 48000 -ac 2 -acodec aac -ab 128k   '$destfilename
 echo "Executing:: $commandString"
 $commandString
  
