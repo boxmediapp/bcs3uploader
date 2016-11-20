1139b022c978044d7e56ac383df8d7d2568c88c3
Dilshat Hewzulla <hewzulla@gmail.com>
2016-11-20 20:48:09 +0000

    developing

--- mov_to_mp4_to_s3.sh
+++ mov_to_mp4_to_s3.sh
@@ -4,7 +4,7 @@ scriptbasedir=$(dirname "$0")
 bucketname=$1
 filepath=$2
 filename=$3
-destfolder=$4
+destfullpath=$4
 
 file_extension="${filename##*.}"
 base_filename="${filename%.*}"
@@ -23,16 +23,23 @@ convert(){
 destfilename=$base_filename'.mp4'
 
 
-
 #commandString='ffmpeg -i '$filename' -y -preset medium -r 25 -s 1024x576 -aspect 16:9 -vcodec libx264 -b:v 11M -profile:v high -level:v 3.1 -vf "yadif=0:-1:0" -af dynaudnorm=f=24:g=31:p=.70:m=20:r=1:n=1:c=1:b=0 -ar 48000 -ac 2 -acodec aac -ab 128k '$destfilename
-commandString='ffmpeg -i '$filename' -y -vcodec copy -af dynaudnorm=f=24:g=31:p=.70:m=20:r=1:n=1:c=1:b=0 -ar 48000 -ac 2 -acodec aac -ab 128k   '$destfilename
+commandString='ffmpeg -i '$filename' -y -vcodec copy -af dynaudnorm=f=24:g=31:p=.70:m=20:r=1:n=1:c=1:b=0 -ar 48000 -ac 2 -acodec aac -ab 128k  -strict -2 '$destfilename
 echo "Executing:: $commandString"
 $commandString
  
-commandString='aws s3 cp '$destfilename' s3://'$bucketname'/'$destfolder'/'$destfilename
-$commandString
+ 
+
+
+commandString='aws s3 cp '$destfilename' '$destfullpath
+
 echo "Executing:: $commandString"
+
+$commandString
+
+
 rm $destfilename
+
 }
 
 
