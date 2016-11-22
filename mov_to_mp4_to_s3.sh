a03218a619653a7e5622837dc1554df24403ce3e
Dilshat Hewzulla <hewzulla@gmail.com>
2016-11-22 00:07:58 +0000

    bc

--- mov_to_mp4_to_s3.sh
+++ mov_to_mp4_to_s3.sh
@@ -3,20 +3,9 @@ scriptbasedir=$(dirname "$0")
 
 sourcevideo=$1
 filename=$2
-destfullpath=$3
-
-
 cd /data
-
-
 commandString='ffmpeg -i '$sourcevideo' -y -vcodec copy -af dynaudnorm=f=24:g=31:p=.70:m=20:r=1:n=1:c=1:b=0 -ar 48000 -ac 2 -acodec aac -ab 128k  -strict -2 '$filename
 echo "Executing:: $commandString"
 $commandString
+echo "complete"
  
-commandString='aws s3 cp '$filename' '$destfullpath
-
-echo "Executing:: $commandString"
-
-$commandString
-
-rm $filename
---
+++ upload_to_s3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+scriptbasedir=$(dirname "$0")
+sourcevideo=$1
+destfullpath=$2
+cd /data
+commandString='aws s3 cp '$sourcevideo' '$destfullpath
+echo "Executing:: $commandString"
+$commandString
+rm $sourcevideo
