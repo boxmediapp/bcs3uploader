721eafc47ed306b20164b1f6192732755fc182a1
Dilshat Hewzulla <hewzulla@gmail.com>
2016-11-22 00:17:10 +0000

    transfer log

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -3,7 +3,7 @@ scriptbasedir=$(dirname "$0")
 sourcevideo=$1
 destfullpath=$2
 cd /data
-commandString='aws s3 cp '$sourcevideo' '$destfullpath
+commandString='aws s3 cp '$sourcevideo' '$destfullpath 
 echo "Executing:: $commandString"
-$commandString
+$commandString >transfer.log
 rm $sourcevideo
